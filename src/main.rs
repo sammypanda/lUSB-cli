380b6c6f732d98c8779f7143301c95bfa6635be8
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-07-05 15:25:06 +1000

    added cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use clap::{Command};
+
 mod cli;
 
 fn main() {
-    cli::cli_devices_list::demo() // lists all visible USB devices
+    let mut cmd = Command::new(env!("CARGO_PKG_NAME"))
+        .arg_required_else_help(true)
+        .version(env!("CARGO_PKG_VERSION", "Version not set"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            Command::new("list")
+                .about("List all recognised USB devices")
+        );
+
+    let matches = cmd.get_matches_mut();
+
+    match matches.subcommand_name() {
+        Some("list") => {
+            cli::cli_devices_list::demo();
+        },
+        _ => {} // required by 'match'
+    }
 }
