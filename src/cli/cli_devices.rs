9ce431dbd2b3086ebc73ee4c212659cfb8a321be
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-07-09 19:51:03 +1000

    created developer 'disable verb' identifier parser
    would be a good idea to move as much of it as possible out of the main.rs into a dedicated identifier parser, maybe in cli_devices.rs

--- src/cli/cli_devices.rs
+++ src/cli/cli_devices.rs
@@ -1,4 +1,52 @@
+extern crate rusb;
 use super::cli_devices_list;
+use std::io::{Error, ErrorKind};
+use rusb::{Context, UsbContext, DeviceHandle};
+
+pub struct Device {
+    index: u8
+}
+
+impl Device {
+    pub fn new(index: u8) -> Self {
+        Self {
+            index
+        }
+    }
+
+    pub fn get_index(&self) -> Result<u8, Error> {
+        Ok(self.index)
+    }
+
+    pub fn get_device_handle(&self) -> Result<DeviceHandle<Context>, Error> {
+        let index = self.index;
+        let context = Context::new().unwrap();
+        let devices = context.devices().unwrap();
+
+        let device = match devices.iter().nth(index as usize) { // handle <Option>
+            Some(device) => device, // pass on the variable
+            None => { // error case
+                return Err(Error::new(
+                    ErrorKind::NotFound,
+                    format!("USB with the '{}' identifier was not found", index)
+                ));
+            }
+        };
+
+        let device_handle = match device.open() { // handle <Result>
+            Ok(device_handle) => device_handle, // pass on the variable
+            Err(_) => { // error case
+                return Err(Error::new(
+                    ErrorKind::Unsupported,
+                    format!("USB device not openable: identifier '{}'", index)
+                ));
+            }
+        };
+
+        // return device_handle as Result
+        Ok(device_handle)
+    }
+}
 
 pub fn list() {
     cli_devices_list::demo();
--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,24 @@ fn main() {
             cli_devices::list();
         },
         Some(("disable", sub_m)) => {
-            println!("Devices listed: {}", sub_m.get_one::<String>("identifiers").unwrap());
+            let identifier_list = match sub_m.get_one::<String>("identifiers") {
+                Some(value) => value
+                    .split(',') // turn into comma separated list
+                    .map(|attempt| attempt.parse::<u8>().ok()) // convert to u8
+                    .filter_map(|result| result) // remove non-u8
+                    .collect(),
+                None => Vec::new(),
+            };
+
+            println!("devices found: {:?}", identifier_list);
+
+            let device_list = identifier_list
+                .into_iter() // iter() is reference to; into_iter() changes type
+                .map(|compatible| cli::cli_devices::Device::new(compatible)); // create instances for each cli_device
+
+            for device in device_list {
+                println!("Device: {:?}", device.get_index().unwrap());
+            }
         }
         _ => {} // required by 'match'
     }
