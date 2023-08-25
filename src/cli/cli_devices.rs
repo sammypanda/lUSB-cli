9d3cf84924b26cc511dbe1e8505a15a7ba5eda34
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-08-25 16:00:53 +1000

    [todo] disable USB device business logic

--- src/cli/cli_devices.rs
+++ src/cli/cli_devices.rs
@@ -17,7 +17,7 @@ impl Device {
     pub fn get_index(&self) -> Result<u8, Error> {
         Ok(self.index)
     }
-
+    
     pub fn get_device_handle(&self) -> Result<DeviceHandle<Context>, Error> {
         let index = self.index;
         let context = Context::new().unwrap();
@@ -54,7 +54,7 @@ pub fn list() {
 
 pub fn handle_verb(verb: &str, device: &Device) {
     match device.get_device_handle() {
-        Ok(device_handle) => {
+        Ok(mut device_handle) => {
             println!("Device: {:?}", device.get_index().unwrap());
             println!(
                 "device_handle: {}",
@@ -64,21 +64,36 @@ pub fn handle_verb(verb: &str, device: &Device) {
                 .unwrap()
                 .product_id()
             );
+            
+            match verb {
+                "enable" => {
+                    // Handle "enable" verb
+                }
+                "disable" => {
+                    // TODO: actually get the correct interface instead of "0", might have to
+                        // -- according to my research most USB devices will just have the one, so 0 is correct, but... 
+                        //
+                        // get device()
+                        // get device config_descriptor()
+                        // unpack result/err
+                        // get configdescriptor interfaces()
+                        // .Iter interfaces so it -> interface
+                        // interface num
+                        // .. uncommented code cont, replace 0 with var for iter ..
+                        //
+                        // something like: device_handle.device().active_config_descriptor().unwrap().interfaces.count()
+                        // (but with an iter instead of the count thing)
+                        //
+                    device_handle.claim_interface(0);
+                    device_handle.detach_kernel_driver(0).unwrap_or_else(|error| println!("{error}"));
+                }
+                _ => {
+                    panic!("Invalid verb, use disable or enable");
+                }
+            }
         },
         Err(error) => {
             eprintln!("{error}");
         }
-    }
-
-    match verb {
-        "enable" => {
-            // Handle "enable" verb
-        }
-        "disable" => {
-            // Handle "disable" verb
-        }
-        _ => {
-            panic!("Invalid verb, use disable or enable");
-        }
-    }
+    };
 }
